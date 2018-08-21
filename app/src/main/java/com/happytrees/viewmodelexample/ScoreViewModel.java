e74b01b5e13c94608c5d6cd7d2e6e025f68a069e
limudimwork <boriskunda@gmail.com>
2018-08-21 19:09:55 +0300

    Initial commit

---
+++ app/src/main/java/com/happytrees/viewmodelexample/ScoreViewModel.java
@@ -0,0 +1,13 @@
+package com.happytrees.viewmodelexample;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ScoreViewModel extends ViewModel {
+
+    // Tracks the score for Team A
+    public int scoreTeamA = 0;
+
+    // Tracks the score for Team B
+    public int scoreTeamB = 0;
+
+}
